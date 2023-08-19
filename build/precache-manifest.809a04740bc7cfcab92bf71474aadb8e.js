7ed1f841bd229f05b80f89f2f8684105641988e0
tylerhu3 <tylerhu@yahoo.com>
2023-08-19 01:30:17 -0700

    Fixed time issue

---
+++ build/precache-manifest.809a04740bc7cfcab92bf71474aadb8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "817be16235e49aa2fb4b",
+    "url": "/static/css/main.3f9be0cf.chunk.css"
+  },
+  {
+    "revision": "817be16235e49aa2fb4b",
+    "url": "/static/js/main.817be162.chunk.js"
+  },
+  {
+    "revision": "67591a87fed3094ab2a8",
+    "url": "/static/js/1.67591a87.chunk.js"
+  },
+  {
+    "revision": "d954de302439292064b5",
+    "url": "/static/css/2.c14a70d6.chunk.css"
+  },
+  {
+    "revision": "d954de302439292064b5",
+    "url": "/static/js/2.d954de30.chunk.js"
+  },
+  {
+    "revision": "5e99d2a528e5c2c001c1",
+    "url": "/static/js/runtime~main.5e99d2a5.js"
+  },
+  {
+    "revision": "c354ff15a45dc18a1029dfed1509dec7",
+    "url": "/static/media/next.c354ff15.mp3"
+  },
+  {
+    "revision": "a615768fc1497ec1dbdcb4fe5fed90a8",
+    "url": "/index.html"
+  }
+];
